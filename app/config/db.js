1a9c502d441dda48178d0052917fa861f73a6cb0
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-03-31 15:55:11 -0400

    Switch to different development database.

--- app/config/db.js
+++ app/config/db.js
@@ -5,7 +5,7 @@ var knex = require('knex')({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     password: process.env.DB_PASS,
-    database: 'mg-dev',
+    database: 'mgcrud',
   }
 });
 var Database = require('bookshelf')(knex);
