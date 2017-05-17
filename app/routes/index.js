e45e00214c432628d672aba6d317a535f67cbb63
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-05-17 11:45:29 -0400

    Add title to home page render

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,17 +11,18 @@ router.use('/admin', auth.isLoggedIn, require('./admin'));
 router.use('/users', auth.isLoggedIn, require('./users'));
 
   // the data routes
-router.use('/locations', auth.isLoggedIn, require('./locations'));
-router.use('/tools', auth.isLoggedIn, require('./tools'));
 router.use('/categories', auth.isLoggedIn, require('./categories'));
+router.use('/locations', auth.isLoggedIn, require('./locations'));
+router.use('/people', auth.isLoggedIn, require('./people'));
+router.use('/tools', auth.isLoggedIn, require('./tools'));
 
 /* Routes of domain name http://domainname.com/xyz */
   // Home page
 router.get('/', auth.isLoggedIn, function(req, res, next) { 
   var userObj = req.user || null;
-  res.render('index', {userInfo: userObj}); 
+  res.render('index', {userInfo: userObj, title: 'MakerGrounds @ UVA'}); 
 });
-
+  // Authentication
 router.get('/login', function(req, res, next) { res.render('login'); });
 
 router.get('/logout', function(req, res, next) { 
