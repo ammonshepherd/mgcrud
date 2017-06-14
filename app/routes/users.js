684e75a953456d79101dbb6b22cfc3e852c4b7f0
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-06-14 16:15:56 -0400

    Add main user route.

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,6 +5,7 @@ var upload = require('../helpers/upload');
 var users = require('../controllers/users');
 
 /* GET users listing. */
+router.get('/', users.list);
 router.get('/:user', users.single);
 
 router.post('/:user', upload.single('avatar'), users.update);
