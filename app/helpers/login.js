de720f7ea9f1b3340e90b0e73f63a63993317390
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-06-26 11:47:25 -0400

    Play with color scheme. Add check for userInfo in navbar.

--- app/helpers/login.js
+++ app/helpers/login.js
@@ -6,8 +6,12 @@ module.export = function (passport) {
     function(username, password, cb) {
       db.users.findByUsername(username, function(err, user) {
         if (err) { return cb(err); }
-        if (!user) { return cb(null, false); }
-        if (user.password != password) { return cb(null, false); }
+        if (!user) { 
+          return cb(null, false, {message: 'Incorrect username'}); 
+        }
+        if (user.password != password) { 
+          return cb(null, false, {message: 'Incorrect password'}); 
+        }
         return cb(null, user);
     });
   }));
