8ae0f4c7972b0fe9c3c1a8d74bdd0610b31cad05
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-06-28 11:07:52 -0400

    Use the same syntax as the users, add instead of edit, add file processing on post.

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,16 +1,17 @@
 /* This route file handles all URL's that go http://domainname/people/ */
 var express = require('express');
 var router = express.Router();
+var upload = require('../helpers/upload');
 
 var peopleController = require('../controllers/people');
 
-router.get('/', peopleController.listAll );
+router.get('/', peopleController.list );
 
-router.get('/edit/', peopleController.edit);
-router.post('/edit/', peopleController.upsert);
+router.get('/add', peopleController.edit);
+router.post('/add', upload.single('avatar'), peopleController.upsert);
 
 router.get('/edit/:id', peopleController.edit);
-router.post('/edit/:id', peopleController.upsert);
+router.post('/edit/:id', upload.single('avatar'), peopleController.upsert);
 
 router.get('/delete/:id', peopleController.delete);
 
