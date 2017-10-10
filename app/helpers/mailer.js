0d0a7ae3aa757ad124091b528e2d2524cfa4dafd
Ammon Shepherd <ammon.shepherd@gmail.com>
2017-10-10 16:10:24 -0400

    Switch smtp to local sendmail.

--- app/helpers/mailer.js
+++ app/helpers/mailer.js
@@ -2,11 +2,9 @@ require('dotenv').config();
 var nodemailer = require('nodemailer');
 
 var mailer = nodemailer.createTransport ({
-  service: 'Gmail',
-  auth: {
-    user: process.env.MAILER_EMAIL,
-    pass: process.env.MAILER_PASS
-  }
+  sendmail: true,
+  newline: 'unix',
+  path: '/usr/sbin/sendmail'
 });
 
 module.exports = mailer;
